5c5db03bc95df0556c8494d86cd5dc7fa9154cac
Shariq-90 <fa18-bcs-036@student.comsats.edu.pk>
2021-04-13 22:53:17 +0500

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! My Name Is Dolat Hamza</Text>
+      <Text>Open up App.js to start working on your app! My Name Is Dolat</Text>
       <StatusBar style="auto" />
     </View>
   );
